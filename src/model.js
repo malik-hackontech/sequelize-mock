f32a4e91e2cc35adacaa295a3a48f94dbfd516e8
melaniedworak <melanie.dworak@foyerapp.io>
2020-06-29 10:35:51 -0400

    brought .sequelize property out to top level

--- src/model.js
+++ src/model.js
@@ -60,6 +60,9 @@ function fakeModel (name, defaults, opts) {
 		]
 	), opts || {});
 	
+	//Pull this out for drop-in support in Foyer's testing -- needed for raw queries
+	this.sequelize = this.options.sequelize;
+	
 	/**
 	 * Name given to the model on initialization
 	 * 
